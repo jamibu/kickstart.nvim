cddd02b1f82ae48a48db60a21567ef77aeac34d7
Jack <jack.bucher@orygen.org.au>
2024-05-17 19:31:59 +1000

    Add blame and jupynium

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,4 +13,16 @@ return {
     },
     config = true,
   },
+  {
+    'kiyoon/jupynium.nvim',
+    build = 'pip3 install --user .',
+    -- build = "conda run --no-capture-output -n jupynium pip install .",
+    -- enabled = vim.fn.isdirectory(vim.fn.expand "~/miniconda3/envs/jupynium"),
+  },
+  {
+    'FabijanZulj/blame.nvim',
+    config = function()
+      require('blame').setup()
+    end,
+  },
 }
