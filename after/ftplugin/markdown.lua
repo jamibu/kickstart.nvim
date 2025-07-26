27d4bae779716dd1dba1bad7e89f026a8e57d3a8
jamibu <bucher.codes@gmail.com>
2025-07-27 01:57:33 +1000

    Adding obsidian.nvim

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,17 @@
+vim.opt_local.conceallevel = 2
+
+-- Wrapping for readability
+vim.opt_local.wrapmargin = 90
+vim.opt_local.textwidth = 90
+vim.opt_local.linebreak = true
+
+-- Tabs
+vim.opt_local.expandtab = false
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+
+-- Spell check
+vim.opt_local.spell = true
+
+-- Remove the colour column
+vim.opt_local.colorcolumn = '0'
